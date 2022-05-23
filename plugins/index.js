57b8fbaac7b660bb9b9e038951187d02b1476193
Siddhant Shukla <70441430+Siddhantshukla814@users.noreply.github.com>
2022-05-23 17:11:06 +0530

    Local plugin test 13

--- plugins/index.js
+++ plugins/index.js
@@ -1,10 +1,30 @@
-// index.js
+const WebPageTest = require("webpagetest");
+
+const wpt = new WebPageTest("https://www.webpagetest.org", "12581d97-7b8b-4519-b02f-b404f401a973");
 
 module.exports = {
   onPostBuild: ({ netlifyConfig }) => {
     console.log(netlifyConfig.build.environment.DEPLOY_PRIME_URL);
+
+    const testURL = netlifyConfig.build.environment.DEPLOY_PRIME_URL;
+
+    let options = {
+      firstViewOnly: true,
+      runs: 1,
+      pollResults: 5,
+    };
+
+    // Run the test
+    wpt.runTest(testURL, options, (err, result) => {
+      if (result) {
+        console.log(result);
+      } else {
+        console.log(err);
+      }
+    });
+
     console.log(
-      "netlifyConfig.build.environment.DEPLOY_PRIME_URL------------------------------------------"
+      "---------------------------------------------------------------------------------------"
     );
   },
 };
