3b5605f80d791b4f394aac2a5640030b05859934
Siddhant Shukla <70441430+Siddhantshukla814@users.noreply.github.com>
2022-05-24 20:17:29 +0530

    Local plugin test 15

--- plugins/index.js
+++ plugins/index.js
@@ -3,10 +3,10 @@ const WebPageTest = require("webpagetest");
 const wpt = new WebPageTest("https://www.webpagetest.org", "12581d97-7b8b-4519-b02f-b404f401a973");
 
 module.exports = {
-  onPostBuild: ({ netlifyConfig }) => {
-    console.log(netlifyConfig.build.environment.DEPLOY_PRIME_URL);
+  onPostBuild: async ({ netlifyConfig }) => {
+    //console.log(netlifyConfig.build.environment.DEPLOY_PRIME_URL)
 
-    const testURL = netlifyConfig.build.environment.DEPLOY_PRIME_URL;
+    const testURL = "https://www.amazon.com";
 
     let options = {
       firstViewOnly: true,
@@ -14,17 +14,39 @@ module.exports = {
       pollResults: 5,
     };
 
-    // Run the test
-    wpt.runTest(testURL, options, (err, result) => {
-      if (result) {
-        console.log(result);
-      } else {
-        console.log(err);
-      }
-    });
+    const runTest = (wpt, url, options) => {
+      return new Promise((resolve, reject) => {
+        console.log(`Submitting test for ${url}...`);
+        wpt.runTest(url, options, async (err, result) => {
+          try {
+            if (result) {
+              return resolve(result);
+            } else {
+              return reject(err);
+            }
+          } catch (e) {
+            console.info(e);
+          }
+        });
+      });
+    };
 
-    console.log(
-      "--------------------------------------------------------------------------------------"
-    );
+    (async function () {
+      Promise.all(
+        urls.map(async (url) => {
+          try {
+            await runTest(wpt, url, options).then(async (result) => {
+              if (result.data) {
+                console.log("asdasdasd");
+              }
+            });
+          } catch (e) {
+            console.error(e);
+          }
+        })
+      ).then(() => {
+        console.info("finalResults");
+      });
+    })();
   },
 };
