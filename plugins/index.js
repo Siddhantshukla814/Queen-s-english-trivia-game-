c0e7e7a4fd64eaac0ea6e4d8e53af88c05213417
Siddhant Shukla <70441430+Siddhantshukla814@users.noreply.github.com>
2022-05-27 18:22:13 +0530

    Local plugin test 43

--- plugins/index.js
+++ plugins/index.js
@@ -2,7 +2,7 @@ const WebPageTest = require("webpagetest");
 const { runTest } = require("./wptHelpers");
 
 module.exports = {
-  onPostBuild: async ({ netlifyConfig, inputs, utils }) => {
+  onPreBuild: async ({ netlifyConfig, inputs, utils }) => {
     console.log("🔥🔥Warming Up The WebPageTest🔥🔥");
 
     const wpt = new WebPageTest("https://www.webpagetest.org", netlifyConfig.build.environment.WPT_API_KEY);
